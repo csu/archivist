986ff5e32888cffd8a1217467fc1b630472bc7ca
Christopher Su <christophersu9@gmail.com>
2016-07-14 23:32:39 -0700

    template

---
+++ archivist.py
@@ -0,0 +1,8 @@
+from __future__ import unicode_literals
+# don't convert to ascii in py2.7 when creating string to return
+
+crontable = []
+outputs = []
+
+def process_message(data):
+  # do something with data
